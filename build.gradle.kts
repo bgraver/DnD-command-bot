10ab43a3d4eebe32e9c4f9465a05bae23d22c10a
bgraver <graver.brandon@gmail.com>
2020-11-22 14:38:34 -0600

    fixing build and gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,5 @@ dependencies {
     implementation("net.dv8tion:JDA:4.2.0_204")
     implementation("org.litote.kmongo:kmongo:4.1.2")
     implementation("org.slf4j:slf4j-simple:1.7.30")
-    implementation("com.beust:klaxon:5.4")
   testImplementation("org.testng:testng:7.3.0")
 }
